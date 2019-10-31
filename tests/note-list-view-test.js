08054169f3a20cfda8d8d7bded7cfbab4aa93ac8
Jessica Marais <jessica_marais@hotmail.com>
2019-10-31 10:44:24 +0000

    Update note list view test

--- tests/note-list-view-test.js
+++ tests/note-list-view-test.js
@@ -2,21 +2,26 @@
 
   function testViewMultipleNotes() {
 
+    ID.reset();
+
     var noteList = new NoteList()
     noteList.newNote("Note")
     noteList.newNote("Note 2")
 
     var viewNoteList = new ViewNoteList(noteList)
-    assert.isTrue(viewNoteList.outputHtml()) === "<ul><li><div>Note</div></li><li><div>Note 2</div></li></ul>"
+    assert.isTrue(viewNoteList.outputHtml()) === "<ul><li><div><a href=#notes/0>Note</a></div></li><li><div><a href=#notes/1>Note 2</a></div></li></ul>"
   }
 
+
   function testViewOneNote() {
+
+    ID.reset();
 
     var noteList = new NoteList()
     noteList.newNote("Note")
 
     var viewNoteList = new ViewNoteList(noteList)
-    assert.isTrue(viewNoteList.outputHtml()) === "<ul><li><div>Note</div></li><li></ul>"
+    assert.isTrue(viewNoteList.outputHtml()) === "<ul><li><div><a href=#notes/0>Note</a></div></li><li></ul>"
   }
 
   function testViewNoNotes() {
@@ -29,11 +34,13 @@
 
   function testLimitCharacters() {
 
+    ID.reset();
+
     var noteList = new NoteList()
     noteList.newNote("Favourite food: pesto")
 
     var viewNoteList = new ViewNoteList(noteList)
-    assert.isTrue(viewNoteList.outputHtml()) === "<ul><li><div>Favourite food: pes...</div></li></ul>"
+    assert.isTrue(viewNoteList.outputHtml()) === "<ul><li><div><a href=#notes/0>Favourite food: pes...</a></div></li></ul>"
   }
 
   function testNoteURL() {
@@ -49,4 +56,5 @@
   testViewOneNote();
   testViewNoNotes();
   testLimitCharacters();
+  testNoteURL();
 })(this)
